77fbb56a88939b0d610a983931d6e9c9426bd535
Roman Istomin <rji5040@psu.edu>
2018-09-09 18:24:45 -0400

    yo get check, comments are in text of your function for last qustion

--- ece_hw1_6.m
+++ ece_hw1_6.m
@@ -11,3 +11,9 @@ error=Y-(X*beta);
 errorsq= error.^2;
 cov=sum((errorsq)/(length(Y)-3))*inv(X'*X);
 stderror= [sqrt(cov(1,1)), sqrt(cov(2,2)), sqrt(cov(3,3)), sqrt(cov(4,4))]
+
+% you need to write all your actions here, if I run this code, it produces
+% nothing because I did not import variables before. you need to write a
+% code in such a way that I only need to press a button and it gives me
+% result. Otherwise, I have to do pre-loading and that kind of stuff.
+% journal referrees will not do that for you.
