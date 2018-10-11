1142f3b60825e584de54c90350a996a878d2b602
DESKTOP\berkidem <ext76@psu.edu>
2018-10-10 22:57:41 -0400

    Revert "HW3 submission"
    This reverts commit 2aa70cf2beb08b4363b087664f9d5c3fac506485.

--- negll.m
+++
@@ -1,6 +0,0 @@
-function negll = negll( beta, X, y ) 
-
-negll =-sum(-exp(X*beta) + X*beta.*y - log(factorial(y)));
-
-end
-
--- ssnls.m
+++
@@ -1,6 +0,0 @@
-function nls = ssnls( beta, X, y ) 
-
-nls =sum((y-exp(X*beta)).^2);
-
-end
-
